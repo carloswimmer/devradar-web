d9ee0a3749c5878cb912a36d61bd16bb6b6b2ad7
carloswimmer <coiote777@gmail.com>
2020-01-17 07:39:04 -0300

    feat: Create form to register developers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+
 function App() {
   return (
-    <h1>Let's code</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário Github</label>
+            <input type="text" name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input type="text" name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input type="number" name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input type="number" name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
